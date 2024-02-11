b0f0be216cb39bc08a6189943687448db857f409
Chandler Duffey <chandlerduffey@MacBook-Pro.lan>
2024-02-10 21:51:02 -0600

    refactor filter

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
-import sys
 from fastapi import FastAPI
 import httpx
 
-from violent import violent
 
 app = FastAPI()
-
-print(sys.path)
 
 
 # generate a formatted date cutoff for a SoQL query
@@ -46,6 +42,26 @@ def make_path(data: dict):
         pass
 
     try:
+        vi = data['violent']
+        if vi:
+            violent = f" AND ucr_code in("\
+                      f"'100', '101', '107', '400', '401', "\
+                      f"'200', '201', '202', '203', '204', '205', '206', '207', '208', "\
+                      f"'300', '301', '302', '303', '304', '305', "\
+                      f"'402', '403', '404', '405', '406', '407', '408', '409', '410', '411', "\
+                      f"'805', '900', '901', '902', '903', '906', '909', '910', '911', "\
+                      f"'1700', '1701', '1702', '1703', '1707', '1708', '1709', '1710', '1711', '1712', '1713', "\
+                      f"'1714', '1715', '1716', '1717', '1718', '1719', '1720', '1721', '1722', '1723', '1724', "\
+                      f"'2004', '2010', '2011', '2012', '2013', '2014', "\
+                      f"'2404', '2406', '2407', '2408', '2409', '2410', "\
+                      f"'2701', '2702', '2703', '2704', '2705', '2706', '2731', '2732', "\
+                      f"'2800', '2801', '2802', '2803', '2804', '2805', "\
+                      f"'4199')"
+            path += violent
+    except KeyError:
+        pass
+
+    try:
         li = data['limit'] 
         limit = f'&$limit={li}'
         path += limit
@@ -56,48 +72,40 @@ def make_path(data: dict):
 
 
 # absolute crime report count over two years
-@app.get('/absolute/{zip}/{co}')
-async def count_all(zip: int, co: int):
-
-    async with httpx.AsyncClient() as client:
-        data = {
-            'zip_code': zip,
-            'select': 'count(incident_report_number)',
-            'cutoff': co
-        }
-        path = make_path(data)
-        response = await client.get(path)
-        match response.status_code:
-            case 200:
-                r = response.json()[0]
-            case _:
-                r = {'status_code': response.status_code}
-
-    # response.json() is a list of JSON objects
-    # return the first (and only) object
+@app.get('/count/{zip}/{co}')
+def count_all(zip: int, co: int):
+    data = {
+        'zip_code': zip,
+        'select': 'count(incident_report_number)',
+        'cutoff': co, 
+    }
+    path = make_path(data)
+    response = httpx.get(path)
+    match response.status_code:
+        case 200:
+            r = response.json()[0]
+        case _:
+            r = {'status_code': response.status_code}
     return r
 
 
 # return a count of 'violent' crimes
-@app.get('/absolute/violent/{zip}/{co}')
-async def count_violent(zip: int, co: int):
-    
-    async with httpx.AsyncClient() as client:
-        lim = await count_all(zip, co)
-        limit = int(lim['count_incident_report_number'])
-        data = {
-            'zip_code': zip,
-            'select': 'ucr_code',
-            'cutoff': co,
-            'limit': limit
-        }
-        path = make_path(data)
-        response = await client.get(path)
-        v = [r['ucr_code'] for r in response.json() if r['ucr_code'] in violent]
-        d = {'count_violent_incident_report_number': str(len(v))} 
-    
-    # FastAPI JSON-encodes the response
-    return d
+@app.get('/count/violent/{zip}/{co}')
+def count_violent(zip: int, co: int):
+    data = {
+        'zip_code': zip,
+        'select': 'count(incident_report_number)',
+        'cutoff': co,
+        'violent': True,
+    }
+    path = make_path(data)
+    response = httpx.get(path)
+    match response.status_code:
+        case 200:
+            r = response.json()[0]
+        case _:
+            r = {'status_code': response.status_code}
+    return r
 
 
 @app.get('/')
